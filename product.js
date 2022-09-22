fadf278e682366534c57b21803d5fba39ce5e64d
LindseySongstad <lindsey.songstad@outook.com>
2022-09-22 10:46:23 -0700

    table in products

--- product.js
+++ product.js
@@ -1,13 +1,24 @@
-var products = [
-    {name: "beer1", type: "ipa"},
-    {name: "beer2", type: "lager"},
-    {name: "beer3", type: "red"}
+var beers = [
+    {name: "beer1", type: "ipa", abv: '7.0'},
+    {name: "beer2", type: "lager", abv: '5.5'},
+    {name: "beer3", type: "red", abv: '5.0'}
 ]
 
 function viewBeers(){
-    for(let i=0; i < products.length; i++){
-        var beer = document.createElement("p");
-        beer.innerHTML = "Beer name: " + products[i].name + " Beer type: " + products[i].type;
-        document.getElementById("beers").appendChild(beer);
+    let headRow = document.createElement('tr');
+    for(let key in beers[0]){
+        let headCol = document.createElement('th');
+        headCol.innerHTML = key;
+        headRow.appendChild(headCol);
+    }
+    document.getElementById("beers").appendChild(headRow);
+    for(let beersI = 0; beersI < beers.length; beersI++ ){
+        let beerRow = document.createElement("tr");
+        for(let key in beers[beersI]){
+            let beerCol = document.createElement("td");
+            beerCol.innerHTML = beers[beersI][key];
+            beerRow.appendChild(beerCol);
+        }
+        document.getElementById("beers").appendChild(beerRow);
     }
 }
