b45ab306e95f1c4a6ecd4459bf898f72380869f0
LindseySongstad <lindsey.songstad@outook.com>
2022-09-22 09:18:34 -0700

    added regex validation to email

--- formValidation.js
+++ formValidation.js
@@ -1,53 +1,67 @@
 
 
 function validateRegister() {
+    let flag = true;
     //first name check 
     if (document.register.Fname.value == "") {
         alert("Please provide your first name");
         document.register.Fname.focus();
-        return false;
+        flag = false;
     } else { var firstNameValue = document.getElementById("fname").value; }
 
     // last name check
     if (document.register.Lname.value == "") {
         alert("Please provide your last name ");
         document.register.Lname.focus();
-        return false;
+        flag = false;
     } else { var lastNameValue = document.getElementById("lname").value; }
 
     // email check
     if (document.register.Email.value == "") {
         alert("Please provide your email ");
         document.register.Email.focus();
-        return false;
+        flag = false;
     } else {
-        if (validateEmail()) {
-            var emailValue = document.getElementById("email").value;
+        var emailValue = document.getElementById("email").value;
+        if (!validateEmail(emailValue)) {
+            document.register.Email.focus();
+            flag = false
         }
-        else { return false }
     }
     // password check
     if (document.register.Pass.value == "") {
         alert("Please provide your password");
         document.register.Pass.focus();
-        return false;
+        flag = false;
     } else {
         var pass = document.getElementById("pass").value;
-        alert("User " + firstNameValue + " " + lastNameValue + "has been registered");
-        return true;
+        alert("User " + firstNameValue + " " + lastNameValue + " has been registered");
     }
+    return flag;
 }
 
-function validateEmail() {
-    var emailValue = document.register.Email.value;
-    atIndex = emailValue.indexOf("@");
-    dotIndex = emailValue.lastIndexOf(".");
-    if (atIndex < 1 || (dotIndex - atIndex < 2)) {
-        alert("Please enter correct email address")
-        document.register.Email.focus();
-        return false;
+function validateEmail (emailAdress)
+{
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAdress.match(regexEmail)) {
+    return true; 
+  } else {
+    alert("Invalid email address");
+    return false; 
+  }
+}
+
+function validateLogin(){
+    let flag = true;
+    var logEmailValue = document.getElementById("logEmail").value;
+    if (!validateEmail(logEmailValue)) {
+        document.login.logEmail.focus();
+        flag = false;
     }
-    else { return true; }
+    else {
+        alert("User "+ logEmailValue + " has been logged in");
+    }
+    return flag;
 }
 
 function toggle(){
@@ -60,12 +74,10 @@ function toggle(){
         showLogin();
     }
 }
-
 function showLogin(){
     document.getElementById("login-container").classList.remove("hidden")
     document.getElementById("register-container").classList.add("hidden")
 }
-
 function showRegister(){
     document.getElementById("login-container").classList.add("hidden")
     document.getElementById("register-container").classList.remove("hidden")
